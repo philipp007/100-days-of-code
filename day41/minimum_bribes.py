a643094e397fdc6cc27363da6eeb75c318574013
Philipp <pengelmann92@googlemail.com>
2018-11-14 21:32:55 +0100

    Day 41 - Hackerrank challenge - Minimum bribes pt2 - finished!

---
+++ day41/minimum_bribes.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def minimum_bribes(q):
+    swap_counts = {}
+    total_count = 0
+    n = len(q)
+
+    for i in range(n):
+        is_sorted = True
+        for j in range(n-i-1):
+            if q[j] > q[j+1]:
+                tmp = q[j]
+                q[j] = q[j+1]
+                q[j+1] = tmp
+                is_sorted = False
+
+                if tmp not in swap_counts:
+                    swap_counts[tmp] = 0
+                swap_counts[tmp] += 1
+                total_count += 1
+
+                if swap_counts[tmp] > 2:
+                    return -1
+        if is_sorted:
+            return total_count
+
+    return total_count
+
+
+if __name__ == '__main__':
+    t = int(input())
+    bribes = []
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        bribes.append(minimum_bribes(q))
+
+    for bribe in bribes:
+        if bribe == -1:
+            print('Too chaotic')
+        else:
+            print(bribe)
